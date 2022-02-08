a8f3b52b32001b89e6abb82140786d092da7a5a7
viizx <viceradicaa@gmail.com>
2022-02-08 16:29:27 -0500

    validation scheme for login and register

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const User = require("../model/User.js");
-
+const { registerValidation } = require("../validation");
 router.post("/register", async (req, res) => {
+  //validate the data before creating the user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
