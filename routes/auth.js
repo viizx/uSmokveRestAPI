c8468eeb1d1b486acb3731467b3da1392376f45a
viizx <viceradicaa@gmail.com>
2022-03-07 16:26:49 -0500

    redirect to frontend after verification

--- routes/auth.js
+++ routes/auth.js
@@ -7,160 +7,170 @@ const verify = require("../verifyToken");
 const Token = require("../model/Token");
 const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
+
+// dobavi korisnika mailom
+router.get("/email/:email", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.params.email });
+        res.json(user);
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
 // dobavi korisnike
 router.get("/", verify, async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.json({ message: error });
-  }
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.json({ message: error });
+    }
 });
 // dobavi korisnika
 router.get("/:userId", async (req, res) => {
-  try {
-    const user = await User.findOne({ _id: req.params.userId });
-    res.json(user);
-  } catch (error) {
-    res.json({ message: error });
-  }
+    try {
+        const user = await User.findOne({ _id: req.params.userId });
+        res.json(user);
+    } catch (error) {
+        res.json({ message: error });
+    }
 });
 // registriraj korisnika
 router.post("/register", async (req, res) => {
-  //validate the data before creating the user
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+    //validate the data before creating the user
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  //check duplicate
-  const emailExists = await User.findOne({ email: req.body.email });
-  if (emailExists) return res.status(400).send("email aldeady exists");
+    //check duplicate
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).send("email aldeady exists");
 
-  //hash the password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    //hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  //create new user
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-    isVerified: false,
-    isAdmin: false,
-  });
+    //create new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        isVerified: false,
+        isAdmin: false,
+    });
 
-  try {
-    let savedUser = await user.save();
+    try {
+        let savedUser = await user.save();
 
-    const token = await new Token({
-      userId: user._id,
-      token: crypto.randomBytes(32).toString("hex"),
-    }).save();
-    const url = `${process.env.BASE_URL}/api/user/${user._id}/verify/${token.token}`;
-    await sendEmail(user.email, "Verify Email", url);
-    res.send({ user: savedUser._id });
-  } catch (error) {
-    res.send(error);
-  }
+        const token = await new Token({
+            userId: user._id,
+            token: crypto.randomBytes(32).toString("hex"),
+        }).save();
+        const url = `${process.env.BASE_URL}/api/user/${user._id}/verify/${token.token}`;
+        await sendEmail(user.email, "Verify Email", url);
+        res.send({ user: savedUser._id });
+    } catch (error) {
+        res.send(error);
+    }
 });
 
 // verifikacija korisnika
 router.get("/:userId/verify/:token", async (req, res) => {
-  try {
-    const user = await User.findOne({ _id: req.params.userId });
-    if (!user) return res.status(400).send({ message: "Invalid Link" });
+    try {
+        const user = await User.findOne({ _id: req.params.userId });
+        if (!user) return res.status(400).send({ message: "Invalid Link" });
 
-    const token = await Token.findOne({
-      userId: user._id,
-      token: req.params.token,
-    });
-    if (!token) return res.status(400).send({ message: "invalid Link" });
+        const token = await Token.findOne({
+            userId: user._id,
+            token: req.params.token,
+        });
+        if (!token) return res.status(400).send({ message: "invalid Link" });
 
-    const updatedUser = await User.updateOne(
-      { _id: user._id },
-      { $set: { isVerified: true } }
-    );
-    await token.remove();
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    res.status(400).send({ message: error });
-  }
+        const updatedUser = await User.updateOne(
+            { _id: user._id },
+            { $set: { isVerified: true } }
+        );
+        await token.remove();
+        res.status(200).redirect('https://port-3001-js-practice-vice889681.codeanyapp.com/');
+    } catch (error) {
+        res.status(400).send({ message: error });
+    }
 });
 
 // zaboravljen password
 
 router.post("/reset", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send({ message: "User doesn't exist" });
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(400).send({ message: "User doesn't exist" });
 
-    const token = await new Token({
-      userId: user._id,
-      token: crypto.randomBytes(32).toString("hex"),
-    }).save();
-    const url = `${process.env.BASE_URL}/api/user/${user._id}/reset/${token.token}`;
-    await sendEmail(user.email, "Reset password", url);
-    res.status(200).json({ message: `Email sent to ${user.email}` });
-  } catch (error) {
-    res.status(400).send({ message: error });
-  }
+        const token = await new Token({
+            userId: user._id,
+            token: crypto.randomBytes(32).toString("hex"),
+        }).save();
+        const url = `${process.env.BASE_URL}/api/user/${user._id}/reset/${token.token}`;
+        await sendEmail(user.email, "Reset password", url);
+        res.status(200).json({ message: `Email sent to ${user.email}` });
+    } catch (error) {
+        res.status(400).send({ message: error });
+    }
 });
 
 // reset password
 
 router.post("/:userId/reset/:token", async (req, res) => {
-  try {
-    //hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const user = await User.findOne({ _id: req.params.userId });
-    if (!user) return res.status(400).send({ message: "Invalid Link" });
+    try {
+        //hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const user = await User.findOne({ _id: req.params.userId });
+        if (!user) return res.status(400).send({ message: "Invalid Link" });
 
-    const token = await Token.findOne({
-      userId: user._id,
-      token: req.params.token,
-    });
-    if (!token) return res.status(400).send({ message: "invalid Link" });
+        const token = await Token.findOne({
+            userId: user._id,
+            token: req.params.token,
+        });
+        if (!token) return res.status(400).send({ message: "invalid Link" });
 
-    const updatedUser = await User.updateOne(
-      { _id: user._id },
-      { $set: { password: hashedPassword } }
-    );
-    await token.remove();
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    res.status(400).send({ message: error });
-  }
+        const updatedUser = await User.updateOne(
+            { _id: user._id },
+            { $set: { password: hashedPassword } }
+        );
+        await token.remove();
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).send({ message: error });
+    }
 });
 // login korisnika
 router.post("/login", async (req, res) => {
-  //validate the data before logging in
-  const { error } = loginValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  //check that email exists
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("email doesn't exist");
-  //password is correct
-  const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send("wrong password");
+    //validate the data before logging in
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send({ message: error.details[0].message });
+    //check that email exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send({ message: "email doesn't exist" });
+    //password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send({ message: "wrong password" });
 
-  if (!user.isVerified) {
-    let token = await Token.findOne({ userId: user._id });
-    if (!token) {
-      const token = await new Token({
-        userId: user._id,
-        token: crypto.randomBytes(32).toString("hex"),
-      }).save();
-      const url = `${process.env.BASE_URL}user/${user._id}/verify/${token.token}`;
-      await sendVerificationEmail(user.email, "Verify Email", url);
+    if (!user.isVerified) {
+        let token = await Token.findOne({ userId: user._id });
+        if (!token) {
+            const token = await new Token({
+                userId: user._id,
+                token: crypto.randomBytes(32).toString("hex"),
+            }).save();
+            const url = `${process.env.BASE_URL}user/${user._id}/verify/${token.token}`;
+            await sendVerificationEmail(user.email, "Verify Email", url);
+        }
+        return status(400).send({
+            message: "An email was sent to you, please verify",
+        });
     }
-    return status(400).send({
-      message: "An email was sent to you, please verify",
-    });
-  }
 
-  //create and assign token
-  const token = await jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header("auth-token", token).send(token);
+    //create and assign token
+    const token = await jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header("auth-token", token).json({ token, user });
 });
 
 module.exports = router;
